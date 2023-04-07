ed35d4a2eeddfc3a45aa0cb6185bce988c44a17a
TikhonenkoVV <113926273+TikhonenkoVV@users.noreply.github.com>
2023-04-07 20:44:09 +0300

    commit task-1

--- src/components/BlogCard/BlogCard.jsx
+++ src/components/BlogCard/BlogCard.jsx
@@ -1,3 +1,59 @@
-export const BlogCard = () => {
-  return <div>BlogCard</div>;
+import PropTypes from 'prop-types';
+import {
+  Card,
+  CardHeader,
+  CardPoster,
+  CardBody,
+  Tag,
+  CardTitle,
+  CardText,
+  CardFooter,
+  UserBox,
+  Avatar,
+  UserInfo,
+  UserName,
+  Date,
+} from './BlogCard.styled';
+import { formatDateToNow } from 'services/format-date';
+
+export const BlogCard = ({
+  name,
+  postedAt,
+  poster,
+  tag,
+  title,
+  description,
+  avatar,
+}) => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardPoster src={poster} alt={title} />
+      </CardHeader>
+      <CardBody>
+        <Tag>{tag}</Tag>
+        <CardTitle>{title}</CardTitle>
+        <CardText>{description}</CardText>
+      </CardBody>
+      <CardFooter>
+        <UserBox>
+          <Avatar src={avatar} alt={name} />
+          <UserInfo>
+            <UserName>{name}</UserName>
+            <Date>{formatDateToNow(postedAt)}</Date>
+          </UserInfo>
+        </UserBox>
+      </CardFooter>
+    </Card>
+  );
+};
+
+BlogCard.propTypes = {
+  name: PropTypes.string.isRequired,
+  postedAt: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
 };
